851424598375130bb38696e927e234f71671a47a
“personal” <ouxrlo3@gmail.com>
2025-01-12 21:36:55 +0900

    13

--- my_first_pjt/articles/urls.py
+++ my_first_pjt/articles/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
-    path('hello/', views.hello),
-    path('data-throw/', views.data_throw),
-    path('data-catch/', views.data_catch),
+    path('hello/', views.hello, name='hello'),
+    path('data-throw/', views.data_throw, name='data-throw'),
+    path('catch/', views.data_catch, name='data-catch'),
 ]

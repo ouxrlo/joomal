e3a454fdf4e6017eb6fc0664ef0cbe6f0af77eaf
“personal” <ouxrlo3@gmail.com>
2025-01-12 23:18:50 +0900

    model_change

--- my_first_pjt/articles/models.py
+++ my_first_pjt/articles/models.py
@@ -5,3 +5,5 @@ from django.db import models
 class Aritcles(models.Model):
     title = models.CharField(max_length=50)
     content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
